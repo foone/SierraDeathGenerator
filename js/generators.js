9c849233828643b888c592b3e0d870f76b36246c
Ioannis KarAvas <io.karavas@gmail.com>
2018-03-08 22:58:43 +0200

    Splitted JS/CSS, Cleaned up index

---
+++ js/generators.js
@@ -0,0 +1,86 @@
+var generators={
+	'bm':{
+		'title': 'Bio Menace',
+		'source':'Apogee Software',
+		'sourceurl':'https://en.wikipedia.org/wiki/3D_Realms',
+		'defaulttext':"  Somewhere in each level, there\nis someone like me who holds a key\nto each exit.  You must find them!"
+	},
+	'sotn':{
+		'title': 'Castlevania: Symphony of the Night',
+		'source':'Konami',
+		'sourceurl':'https://en.wikipedia.org/wiki/Konami',
+		'defaulttext':'Richter\n  Die monster.\n  You don\'t belong\n  in this world!'
+	},
+	'keen4':{
+		'title': 'Commander Keen 4',
+		'source':'id Software',
+		'sourceurl':'https://en.wikipedia.org/wiki/Id_Software',
+		'defaulttext':'Keen enters the\n     Shadowlands'
+	},
+	'dos':{
+		'title': 'MS-DOS',
+		'source':'Microsoft',
+		'sourceurl':'https://en.wikipedia.org/wiki/Microsoft',
+		'defaulttext':"Starting MS-DOS...\n\n\nHIMEM is testing extended memory...done.\nC:\>"
+	},
+	'dn1':{
+		'title': 'Duke Nukem 1',
+		'source':'Apogee Software',
+		'sourceurl':'https://en.wikipedia.org/wiki/3D_Realms',
+		'defaulttext':"You're wrong, Proton\nbreath.  I'll be done\nwith you and still have\ntime to watch Oprah!"
+	},
+	'kq4':{
+		'title': 'King\'s Quest 4',
+		'source':'Sierra Online',
+		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+		'defaulttext':'    "Thank you for playing\n           King\'s Quest IV,\n      `The Perils of Rosella.\'\n\nNext time... be more careful!"'
+	},
+	'mgs':{
+		'title': 'Metal Gear Solid',
+		'source':'Konami',
+		'sourceurl':'https://en.wikipedia.org/wiki/Konami',
+		'defaulttext':"I can't believe I'm being hit on\nby the famous Solid Snake..."
+	},
+	'mh1':{
+		'title': 'Manhunter: New York',
+		'source':'Sierra Online',
+		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+		'defaulttext':"Now, let's back up to a few\nminutes before you made\nyour fatal mistake..."
+	},
+	'pq2':{
+		'title': 'Police Quest 2',
+		'source':'Sierra Online',
+		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+		'defaulttext':'Thank you for playing Police\nQuest 2. Next time, be a little more\ncareful.'
+	},
+	'pq3':{
+		'title': 'Police Quest 3',
+		'source':'Sierra Online',
+		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+		'defaulttext':"Those curbs just sneak right\n    up on you, don't they?"
+	},
+	'sc2k':{
+		'title': 'SimCity 2000 Advisor',
+		'source':'Maxis Software',
+		'sourceurl':'https://en.wikipedia.org/wiki/Maxis',
+		'defaulttext':"YOU CAN'T CUT BACK ON\nFUNDING! YOU WILL\nREGRET THIS!"
+	},
+	'sq3':{
+		'title': 'Space Quest 3',
+		'source':'Sierra Online',
+		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+		'defaulttext':"Thanks for playing Space Quest\n]I[. As usual, you've been a real\n                    hoot."
+	},
+	'sq4':{
+		'title': 'Space Quest 4',
+		'source':'Sierra Online',
+		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+		'defaulttext':'Just as you fade\nfrom the living \norganism club you\nthink, in\namazement, "So that\'s\nwhat my spleen looks\nlike!"'
+	},
+	'zerowing':{
+		'title': 'Zero Wing',
+		'source':'Toaplan',
+		'sourceurl':'https://en.wikipedia.org/wiki/Toaplan',
+		'defaulttext':"Cats: All your base are belong\n      to us."
+	}
+}
