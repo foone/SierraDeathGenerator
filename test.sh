98a2cd566faa28b95c392b71d7ab73a6d9d94804
Ioannis KarAvas <io.karavas@gmail.com>
2018-03-08 22:14:23 +0200

    Organizing game files

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+mkdir bm
+mv bm* bm/
+mkdir dn1
+mv dn1* dn1/
+mkdir dos
+mv dos* dos/
+mkdir keen4
+mv keen4* keen4/
+mkdir kq4
+mv kq4* kq4/
+mkdir mgs
+mv mgs* mgs/
+mkdir mh1
+mv mh1* mh1/
+mkdir pq2
+mv pq2* pq2/
+mkdir pq3
+mv pq3* pq3/
+mkdir sc2k
+mv sc2k* sc2k/
+mkdir sotn
+mv sotn* sotn/
+mkdir sq3
+mv sq3* sq3/
+mkdir sq4
+mv sq4* sq4/
+mkdir zerowing
+mv zerowing* zerowing/
