e80a3e8fead063033f46029c966f34ca359dd736
ioannis <io.karavas@gmail.com>
2018-04-06 08:41:17 +0300

    Implemented mithril updates / Canvas

--- js/generators.js
+++ js/generators.js
@@ -1,101 +1,105 @@
-let generators=[
-	{
-		'key' : 'baddudes',
-		'title': 'Bad Dudes',
-		'source':'Data East',
-		'sourceurl':'https://en.wikipedia.org/wiki/Data_East',
-		'defaulttext':" The president has been\n\nkidnapped by Ninjas.\n\n\n Are you a bad enough dude\n\nto rescue the president?"
-	},
-	{
-	    'key' : 'bm',
-		'title': 'Bio Menace',
-		'source':'Apogee Software',
-		'sourceurl':'https://en.wikipedia.org/wiki/3D_Realms',
-		'defaulttext':"  Somewhere in each level, there\nis someone like me who holds a key\nto each exit.  You must find them!"
-	},
-	{   'key' : 'sotn',
-		'title': 'Castlevania: Symphony of the Night',
-		'source':'Konami',
-		'sourceurl':'https://en.wikipedia.org/wiki/Konami',
-		'defaulttext':'Richter\n  Die monster.\n  You don\'t belong\n  in this world!'
-	},
-	{   'key': 'keen4',
-		'title': 'Commander Keen 4',
-		'source':'id Software',
-		'sourceurl':'https://en.wikipedia.org/wiki/Id_Software',
-		'defaulttext':'Keen enters the\n     Shadowlands'
-	},
-	{   'key' : 'dos',
-		'title': 'MS-DOS',
-		'source':'Microsoft',
-		'sourceurl':'https://en.wikipedia.org/wiki/Microsoft',
-		'defaulttext':"Starting MS-DOS...\n\n\nHIMEM is testing extended memory...done.\nC:\>"
-	},
-	{   'key' : 'dn1',
-		'title': 'Duke Nukem 1',
-		'source':'Apogee Software',
-		'sourceurl':'https://en.wikipedia.org/wiki/3D_Realms',
-		'defaulttext':"You're wrong, Proton\nbreath.  I'll be done\nwith you and still have\ntime to watch Oprah!"
-	},
-	{   'key' : 'kq4',
-		'title': 'King\'s Quest 4',
-		'source':'Sierra Online',
-		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
-		'defaulttext':'    "Thank you for playing\n           King\'s Quest IV,\n      `The Perils of Rosella.\'\n\nNext time... be more careful!"'
-	},
-	{   'key' : 'mh1',
-		'title': 'Manhunter: New York',
-		'source':'Sierra Online',
-		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
-		'defaulttext':"Now, let's back up to a few\nminutes before you made\nyour fatal mistake..."
-	},
-	{   'key' : 'mgs',
-		'title': 'Metal Gear Solid',
-		'source':'Konami',
-		'sourceurl':'https://en.wikipedia.org/wiki/Konami',
-		'defaulttext':"I can't believe I'm being hit on\nby the famous Solid Snake..."
-	},
-	{   'key' : 'mk2',
-		'title': 'Mortal Kombat II',
-		'source':'Midway',
-		'sourceurl':'https://en.wikipedia.org/wiki/Mortal_Kombat_II',
-		'defaulttext':"Can you take the cold?",
-		"contributor": "emmakris",
-	},
-	{   'key' : 'pq2',
-		'title': 'Police Quest 2',
-		'source':'Sierra Online',
-		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
-		'defaulttext':'Thank you for playing Police\nQuest 2. Next time, be a little more\ncareful.'
-	},
-	{   'key' : 'pq3',
-		'title': 'Police Quest 3',
-		'source':'Sierra Online',
-		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
-		'defaulttext':"Those curbs just sneak right\n    up on you, don't they?"
-	},
-	{   'key': 'sc2k',
-		'title': 'SimCity 2000 Advisor',
-		'source':'Maxis Software',
-		'sourceurl':'https://en.wikipedia.org/wiki/Maxis',
-		'defaulttext':"YOU CAN'T CUT BACK ON\nFUNDING! YOU WILL\nREGRET THIS!"
-	},
-	{   'key' : 'sq3',
-		'title': 'Space Quest 3',
-		'source':'Sierra Online',
-		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
-		'defaulttext':"Thanks for playing Space Quest\n]I[. As usual, you've been a real\n                    hoot."
-	},
-	{   'key':'sq4',
-		'title': 'Space Quest 4',
-		'source':'Sierra Online',
-		'sourceurl':'https://en.wikipedia.org/wiki/Sierra_Entertainment',
-		'defaulttext':'Just as you fade\nfrom the living \norganism club you\nthink, in\namazement, "So that\'s\nwhat my spleen looks\nlike!"'
-	},
-	{   'key' : 'zerowing',
-		'title': 'Zero Wing',
-		'source':'Toaplan',
-		'sourceurl':'https://en.wikipedia.org/wiki/Toaplan',
-		'defaulttext':"Cats: All your base are belong\n      to us."
-	}
-];
+let data = {
+
+    default : 'baddudes',
+
+	generators : {
+
+        baddudes: {
+            'title': 'Bad Dudes',
+            'source': 'Data East',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Data_East',
+            'defaulttext': " The president has been\n\nkidnapped by Ninjas.\n\n\n Are you a bad enough dude\n\nto rescue the president?"
+        },
+        bm: {
+            'title': 'Bio Menace',
+            'source': 'Apogee Software',
+            'sourceurl': 'https://en.wikipedia.org/wiki/3D_Realms',
+            'defaulttext': "  Somewhere in each level, there\nis someone like me who holds a key\nto each exit.  You must find them!"
+        },
+        sotn: {
+            'title': 'Castlevania: Symphony of the Night',
+            'source': 'Konami',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Konami',
+            'defaulttext': 'Richter\n  Die monster.\n  You don\'t belong\n  in this world!'
+        },
+        keen4: {
+            'title': 'Commander Keen 4',
+            'source': 'id Software',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Id_Software',
+            'defaulttext': 'Keen enters the\n     Shadowlands'
+        },
+        dos: {
+            'title': 'MS-DOS',
+            'source': 'Microsoft',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Microsoft',
+            'defaulttext': "Starting MS-DOS...\n\n\nHIMEM is testing extended memory...done.\nC:\>"
+        },
+        dn1: {
+            'title': 'Duke Nukem 1',
+            'source': 'Apogee Software',
+            'sourceurl': 'https://en.wikipedia.org/wiki/3D_Realms',
+            'defaulttext': "You're wrong, Proton\nbreath.  I'll be done\nwith you and still have\ntime to watch Oprah!"
+        },
+        kq4: {
+            'title': 'King\'s Quest 4',
+            'source': 'Sierra Online',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+            'defaulttext': '    "Thank you for playing\n           King\'s Quest IV,\n      `The Perils of Rosella.\'\n\nNext time... be more careful!"'
+        },
+        mh1: {
+            'title': 'Manhunter: New York',
+            'source': 'Sierra Online',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+            'defaulttext': "Now, let's back up to a few\nminutes before you made\nyour fatal mistake..."
+        },
+        mgs: {
+            'title': 'Metal Gear Solid',
+            'source': 'Konami',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Konami',
+            'defaulttext': "I can't believe I'm being hit on\nby the famous Solid Snake..."
+        },
+        mk2: {
+            'title': 'Mortal Kombat II',
+            'source': 'Midway',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Mortal_Kombat_II',
+            'defaulttext': "Can you take the cold?",
+            "contributor": "emmakris",
+        },
+        pq2: {
+            'title': 'Police Quest 2',
+            'source': 'Sierra Online',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+            'defaulttext': 'Thank you for playing Police\nQuest 2. Next time, be a little more\ncareful.'
+        },
+        pq3 : {
+            'title': 'Police Quest 3',
+            'source': 'Sierra Online',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+            'defaulttext': "Those curbs just sneak right\n    up on you, don't they?"
+        },
+        sc2k: {
+            'title': 'SimCity 2000 Advisor',
+            'source': 'Maxis Software',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Maxis',
+            'defaulttext': "YOU CAN'T CUT BACK ON\nFUNDING! YOU WILL\nREGRET THIS!"
+        },
+        sq3: {
+            'title': 'Space Quest 3',
+            'source': 'Sierra Online',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+            'defaulttext': "Thanks for playing Space Quest\n]I[. As usual, you've been a real\n                    hoot."
+        },
+        sq4: {
+            'title': 'Space Quest 4',
+            'source': 'Sierra Online',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Sierra_Entertainment',
+            'defaulttext': 'Just as you fade\nfrom the living \norganism club you\nthink, in\namazement, "So that\'s\nwhat my spleen looks\nlike!"'
+        },
+        zerowing: {
+            'title': 'Zero Wing',
+            'source': 'Toaplan',
+            'sourceurl': 'https://en.wikipedia.org/wiki/Toaplan',
+            'defaulttext': "Cats: All your base are belong\n      to us."
+        }
+    }
+};
--- js/main.js
+++ js/main.js
@@ -1,39 +1,187 @@
 floppyImage = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgAQMAAADYVuV7AAAABlBMVEX///8AAABVwtN+AAAAAWJLR0QAiAUdSAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAAd0SU1FB+IBBRQ0G7eG+BwAAACiSURBVDjLzdIxCsMwDAVQgQePOkIuIvDRmqP5Br2CS4eM8ZhBxHUUt8gE09KQtNoefGRbFgAkKQapFbNGqtBreAHbAGBTEEyCMSoM0wvdcGeF26wQksZFw2l0ZwDsB1hGsAfrYP8G7Ysa+ey3DarY7mcfN7ey606vcvoaPymMLRC1gDlIVMIVjM9JKuEKEJGJHG+xRHIL2AIjXo1/nqNxfj0AmAoaBZSWo6oAAAAASUVORK5CYII=";
 
-mainTemplate = m("main",[
-        m("div",{class:"mw9 center ph1-ns"},[
-            m("div",{class:"cf ph1-ns"},[
-                m("div",{class:"fl w-100 w-100-ns pa1"},[
+let utils = {
 
-                    m("h1",{class:"f3 fw6 ttu"},["Sierra Death Generator"]),
-                    m("h2",{class:"f4"},["Generator"]),
+    existingGenerator : function(reqGenerator) {
+        return data.generators.hasOwnProperty(reqGenerator);
+    },
 
-                    m("div",{class:"tracked"},[
-                        generators.map(function(generator){
-                            return [ m("a",{class:"f6 link dim ph3 pv2 mb2 dib white bg-dark-gray", href:'#'+generator.key},[generator.title]), " "]
-                        })
-                    ]),
+    fetchGeneratorData : function(reqGenerator) { // Fetches data for generator
 
-                    m("div",[
-                        m("a",{href:"https://github.com/foone/SierraDeathGenerator"},["Code"])," by ",m("a",{href:"https://twitter.com/Foone"},["@Foone"]),
-                        m("span",{id:"extra-contrib"},[", content by ",m("a",{href:"#"},["somebody"])])
-                    ]),
+        // Get template image (if we don't have it already)
+        if (!data.generators[reqGenerator].hasOwnProperty('template')) {
+            m.request({
+                method: "GET",
+                responseType: 'arraybuffer',
+                url: 'games/' + reqGenerator + '/' + reqGenerator + '-blank.png',
+                config: function(xhr) {
+                    xhr.responseType = 'arraybuffer';
+                },
+                extract: function (xhr) {
+                    return utils.createImageObject(xhr.response)
+                }
+            }).then(function (templateData) {
+                data.generators[reqGenerator].template = templateData
+            })
+        }
 
-                    m("canvas",{id:"death"},["No canvas!"]),
+        // Get font (if we don't have it already)
+        if (!data.generators[reqGenerator].hasOwnProperty('font')) {
+            m.request({
+                method: "GET",
+                url: 'games/' + reqGenerator + '/' + reqGenerator + '-font.png',
+                config: function(xhr) {
+                    xhr.responseType = 'arraybuffer';
+                },
+                extract: function (xhr) {
+                    return utils.createImageObject(xhr.response)
+                }
+            }).then(function (fontData) {
+                data.generators[reqGenerator].font = fontData
+            })
+        }
 
-                    m("p",[
-                        m("textarea",{cols:"40",rows:"6",class:"border-box hover-black measure b--black-20 pa2 mb2"}),
+    },
 
-                        m("a",{href:"#",id:"save"},[
-                            m("img",{src:floppyImage, width:"96", height:"96"})
+    createBase64src : function(imageData){
+        var uInt8Array = new Uint8Array(imageData);
+        var i = uInt8Array.length;
+        var binaryString = new Array(i);
+
+        while (i--) {
+            binaryString[i] = String.fromCharCode(uInt8Array[i]);
+        }
+
+        return "data:image/png;base64,"+window.btoa(binaryString.join(''))
+    },
+
+    createImageObject : function(imageData) {
+        let imgElement = new Image()
+        imgElement.src = utils.createBase64src(imageData)
+        return imgElement
+    },
+
+    resourcesLoaded : function(vnode) {
+        return ((data.generators[ vnode.attrs.generator ].template) && (data.generators[ vnode.attrs.generator ].font))
+    },
+
+    initCanvas : function(vnode) {
+
+        var canvas = document.getElementById('death');
+        var template = data.generators[vnode.attrs.generator].template
+        var font = data.generators[vnode.attrs.generator].font
+
+        canvas.width = template.width
+        canvas.height = template.height
+
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(template, 0, 0);
+        } else {
+            // canvas-unsupported code here
+        }
+    }
+
+};
+
+let mainTemplate = {
+    oninit: function(vnode) {
+        console.log("initialized")
+        if (!utils.existingGenerator(vnode.attrs.generator)){
+            m.route.set('/'+data.default)
+        }
+        utils.fetchGeneratorData(vnode.attrs.generator)
+    },
+    oncreate: function(vnode) {
+        console.log("DOM created")
+    },
+    onupdate: function(vnode) {
+        console.log("DOM updated")
+        if (utils.resourcesLoaded(vnode)) {
+            utils.initCanvas(vnode)
+        }
+    },
+    onbeforeremove: function(vnode) {
+        console.log("exit animation can start")
+        return new Promise(function(resolve) {
+            // call after animation completes
+            resolve()
+        })
+    },
+    onremove: function(vnode) {
+        console.log("removing DOM element")
+    },
+    onbeforeupdate: function(vnode, old) {
+        console.log('Before vnode update')
+        if (!utils.existingGenerator(vnode.attrs.generator)){
+            m.route.set('/'+data.default)
+            return false
+        }
+        utils.fetchGeneratorData(vnode.attrs.generator)
+        return true
+    },
+    view : function(vnode) {
+
+
+        return m("main", [
+            m("p",[vnode.attrs.generator]),
+            m("div", {class: "mw9 center ph1-ns"}, [
+                m("div", {class: "cf ph1-ns"}, [
+                    m("div", {class: "fl w-100 w-100-ns pa1"}, [
+
+                        m("h1", {class: "f3 fw6 ttu"}, ["Sierra Death Generator"]),
+                        m("h2", {class: "f4"}, [data.generators[vnode.attrs.generator].title]),
+
+                        m("div", {class: "tracked"}, [
+                            function(){
+                                generators = []
+                                for(key in data.generators) {
+                                    if(data.generators.hasOwnProperty(key)) {
+                                        generator = data.generators[key]
+                                        generators.push([m("a", {
+                                            class: "f6 link dim ph3 pv2 mb2 dib white bg-dark-gray",
+                                            href: '/' + key,
+                                            oncreate: m.route.link,
+                                        }, [generator.title]), " "]);
+                                    }
+                                }
+                                return generators;
+                            }()
+                        ]),
+
+                        m("div", [
+                            m("a", {href: "https://github.com/foone/SierraDeathGenerator"}, ["Code"]), " by ", m("a", {href: "https://twitter.com/Foone"}, ["@Foone"]),
+                            m("span", {id: "extra-contrib"}, [", content by ", m("a", {href: "#"}, ["somebody"])])
+                        ]),
+
+                        function(){
+                            if (utils.resourcesLoaded(vnode)) {
+                                return m("canvas", {id: "death"}, ["No canvas!"])
+                            }
+                        }(),
+
+                        m("p", [
+                            m("textarea", {
+                                cols: "40",
+                                rows: "6",
+                                class: "border-box hover-black measure b--black-20 pa2 mb2"
+                            }),
+
+                            m("a", {href: "#", id: "save"}, [
+                                m("img", {src: floppyImage, width: "96", height: "96"})
+                            ])
                         ])
-                    ])
 
+                    ])
                 ])
             ])
         ])
-    ]);
+    }
+};
 
 let root = document.body;
 
-m.render(root, mainTemplate);
+m.route(root, "/", {
+    "/" : mainTemplate,
+    "/:generator" : mainTemplate,
+})
