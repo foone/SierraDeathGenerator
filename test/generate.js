ed2ec31603fc22483cf5897b7a726717583c103b
Foone Turing <foone.turing@purestorage.com>
2018-05-15 16:52:24 -0700

    Initial work on testing

---
+++ test/generate.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const readFile = util.promisify(fs.readFile);
+
+
+
+(async () => {
+	const browser = await puppeteer.launch({'args':['--allow-file-access-from-files'],'headless':true});
+	const indexURL = 'file://' + path.resolve(__dirname,'../index.html') + '#';
+	const origPage = await browser.newPage();
+	await origPage.goto(indexURL)
+	var generators = await origPage.evaluate('generators')
+	var output={}
+	for(let generator of Object.keys(generators)){
+			let out = output[generator] = {}
+			const page = await browser.newPage();
+			//page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+			await page.goto(indexURL + generator);
+			await page.click('#hidelink');
+			await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './'});
+			await page.waitForFunction("$('#sourcetext').text('')")
+			await page.type('#sourcetext','hello');
+			var image = await page.evaluate('getDataURLImage()')
+			// ADD OPTIONS IN
+			//
+			out['image']=image
+			out['options'] = await page.evaluate("(function (){let opts = {'main-text':$('#sourcetext').val()};$('select').each(function(_,e){opts[$(e).attr('id').split('-',2)[1]]=$(e).val()});return opts})()")
+	}
+	await browser.close();
+	console.log(JSON.stringify(output))
+})();
