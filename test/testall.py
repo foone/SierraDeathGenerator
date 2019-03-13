9fd9650f3549c82fa3172aeaea807b5aaa22dff4
Foone Turing <fooneturing@gmail.com>
2019-03-13 01:20:52 -0700

    Sort generators before running testall

--- test/testall.py
+++ test/testall.py
@@ -4,6 +4,7 @@ if not os.path.exists(OUTPUT_DIR):
 	os.mkdir(OUTPUT_DIR)
 generators = glob.glob(os.path.join('..', 'games', '*', 'tests'))
 names = [os.path.split(os.path.split(path)[0])[1] for path in generators]
+names.sort()
 for name in names:
 	outpath = os.path.join(OUTPUT_DIR, name+'.html')
 	result = subprocess.check_output([sys.executable, 'testgenerator.py', name, outpath])
