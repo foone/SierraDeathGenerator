dd1fa702fa09252037c346798748040468bd5990
Foone Turing <fooneturing@gmail.com>
2024-11-16 18:05:35 -0800

    py3izing makesmarter.py

--- tools/makesmarter.py
+++ tools/makesmarter.py
@@ -1,14 +1,13 @@
 # encoding=utf-8
 
 import string
-CHARS=(u' !"#$%&\'[]*+,-./0123456789;:←=→?©'+string.ascii_uppercase+
-u'\0_▯↑↓')
+CHARS=(u'ABCDEFGHIJKLMNOPQRSTUVWXYZ.©&™-,/\'')
 
-w,h=8,16
+w,h=8,8
 xspacing=0
 
 for i,c in enumerate(CHARS):
 	if c != '\0':
-		print """"{}": {{
+		print (""""{}": {{
 	  "x": {}
-	}},""".format(ord(c),(w+xspacing)*i,w,h)
+	}},""".format(ord(c),(w+xspacing)*i,w,h))
